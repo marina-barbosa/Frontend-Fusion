74c66f6c6bb67e16cb724a9234753f7a83603522
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-20 03:11:20 -0300

    v2 calculadora

--- react-developer-dio/desafio-calculadora/src/App.js
+++ react-developer-dio/desafio-calculadora/src/App.js
@@ -15,12 +15,13 @@ const App = () => {
     setOperation('')
   };
 
+
   const handleAddNumber = (num) => {
     setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
   }
 
-  const handleSumNumbers = () => {
 
+  const handleSumNumbers = () => {
     if(firstNumber === '0'){
         setFirstNumber(String(currentNumber));
         setCurrentNumber('0')
@@ -30,11 +31,9 @@ const App = () => {
       setCurrentNumber(String(sum))
       setOperation('')
     }
-
   }
 
   const handleMinusNumbers = () => {
-
     if(firstNumber === '0'){
         setFirstNumber(String(currentNumber));
         setCurrentNumber('0')
@@ -44,11 +43,36 @@ const App = () => {
       setCurrentNumber(String(minus))
       setOperation('')
     }
-
   }
 
-  const handleEquals = () => {
 
+  const handleDivisionNumbers = () => {
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('/')
+    }else {
+      const div = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(div))
+      setOperation('')
+    }
+  }
+
+
+  const handleMultplyNumbers = () => {
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('x')
+    }else {
+      const res = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(res))
+      setOperation('')
+    }
+  }
+
+
+  const handleEquals = () => {
     if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
         switch(operation){
           case '+':
@@ -57,11 +81,16 @@ const App = () => {
           case '-':
             handleMinusNumbers();
             break;
+          case '/':
+            handleDivisionNumbers();
+            break;
+          case 'x':
+            handleMultplyNumbers();
+            break;
           default: 
             break;
         }
     }
-
   }
 
   return (
@@ -69,27 +98,33 @@ const App = () => {
       <Content>
         <Input value={currentNumber}/>
         <Row>
-        <Button label="%" onClick ={() => handleAddNumber('')}/>
-        <Button label="/" onClick ={() => handleAddNumber('')}/>
-        <Button label="C" onClick ={handleOnClear}/>
-        <Button label="X" onClick ={() => handleAddNumber('')}/>        
+        <Button label="" onClick ={() => handleAddNumber('')}/>
+        <Button label="" onClick ={() => handleAddNumber('')}/>
+        <Button label="c" onClick ={handleOnClear}/>
+        <Button label="/" onClick ={handleDivisionNumbers}/>       
         </Row>
         <Row>
         <Button label="7" onClick ={() => handleAddNumber('7')}/>
         <Button label="8" onClick ={() => handleAddNumber('8')}/>
         <Button label="9" onClick ={() => handleAddNumber('9')}/>
-        <Button label="-" onClick ={handleMinusNumbers}/>        
+        <Button label="x" onClick ={handleMultplyNumbers}/>         
         </Row>
         <Row>
         <Button label="4" onClick ={() => handleAddNumber('4')}/>
         <Button label="5" onClick ={() => handleAddNumber('5')}/>
         <Button label="6" onClick ={() => handleAddNumber('6')}/>
-        <Button label="+" onClick ={handleSumNumbers}/>
+        <Button label="-" onClick ={handleMinusNumbers}/>
         </Row>
         <Row>
         <Button label="1" onClick ={() => handleAddNumber('1')}/>
         <Button label="2" onClick ={() => handleAddNumber('2')}/>
         <Button label="3" onClick ={() => handleAddNumber('3')}/>
+        <Button label="+" onClick ={handleSumNumbers}/>
+        </Row>
+        <Row>
+        <Button label="" onClick ={() => handleAddNumber('')}/>
+        <Button label="0" onClick ={() => handleAddNumber('0')}/>
+        <Button label="." onClick ={() => handleAddNumber('.')}/>
         <Button label="=" onClick ={handleEquals}/>
         </Row>
       </Content>
